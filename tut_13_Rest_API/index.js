bea7347501ec26dae222cb72b93cf0ab2c47921b
Muhammad-Junaid-Aqib <m.junaidaqib@gmail.com>
2024-01-19 23:37:57 +0500

    Rest Api done

--- tut_13_Rest_API/index.js
+++ tut_13_Rest_API/index.js
@@ -1,30 +1,58 @@
-const express = require('express')
-const users = require('../MOCK_DATA.json')
-const app = express()
-const PORT = 5000
+const express = require("express");
+const fs = require("fs");
+const users = require("../MOCK_DATA.json");
 
 
-app.get('/', (req, res) => {
-    return res.send('Home Page')
-})
+const app = express();
+const PORT = 5000;
 
-app.get('/about', (req, res) => {
-    return res.send('About Page')
-})
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/users', (req, res) => {
-    const html = `
+app.get("/", (req, res) => {
+  return res.send("Home Page");
+});
+
+app.get("/users", (req, res) => {
+  const html = `
     <ul>
         ${users.map((user) => `<li> ${user.first_name} </li>`).join("")}
     </ul>
-    `
-    return res.send(`${html}`)
-})
+    `;
+  return res.send(`${html}`);
+});
 
-app.get('/api/users', (req, res) => {
-    return res.json(users)
-})
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((item) => item.id === Number(req.params.id));
+  return res.json(user);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((item) => item.id === Number(req.params.id));
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    return res.json({ status: "pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ id: users.length + 1, ...body });
+  fs.writeFile("../MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "successful", id: users.length });
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
